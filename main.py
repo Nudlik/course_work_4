1c63016dd56486a3eb8e61ed5f9a229f9b50c03d
Nudlik <gegorp@mail.com>
2023-09-15 03:58:32 +0700

    add search

--- main.py
+++ main.py
@@ -5,7 +5,12 @@ from models.jsonsaver import JsonSaver
 
 
 def main():
-    pass
+    user_find_text = input('Введите слово для поиска по которому будем искать вакансии: ').lower().strip()
+    count_pages = int(input('Введите количество страниц для парсинга(на 1ой странице будет 10 вакансий): ').strip())
+    list_platforms = [HeadHunterAPI, SuperJobAPI]
+    for platform in list_platforms:
+        platform = platform(user_find_text)
+        platform.save_data(count_pages)
 
 
 if __name__ == '__main__':
