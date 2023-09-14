a11c850fafe2facb178d91774f35e82649e38bf5
Nudlik <gegorp@mail.com>
2023-09-15 02:22:24 +0700

    refactor main.p

---
+++ main.py
@@ -0,0 +1,12 @@
+from models.headhunter import HeadHunterAPI
+from models.superjob import SuperJobAPI
+from models.vacancy import Vacancy
+from models.jsonsaver import JsonSaver
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
