47a41c78dc76d809bdf0e782b73bd91603dace5e
Nudlik <gegorp@mail.com>
2023-09-15 03:57:59 +0700

    add search per page in class SuperJobAPI

--- models/superjob.py
+++ models/superjob.py
@@ -4,22 +4,24 @@ from pprint import pprint
 
 import requests
 
-from absclasses import AbstractClassAPI
+from models.absclasses import AbstractClassAPI
 
 
 class SuperJobAPI(AbstractClassAPI):
 
     url = 'https://api.superjob.ru/2.0/vacancies/'
     API_KEY = os.getenv('SUPERJOB_API_KEY')
-    path_to_json = '../data/sj_vacancy.json'
+    path_to_json = os.path.join(os.path.dirname(__file__), '..', 'data', 'sj_vacancy.json')
 
     def __init__(self, keyword):
         self.keyword = keyword
 
-    def get_vacancies(self):
+    def get_vacancies(self, pages=0):
 
         query_parameters = {
-            'keyword': self.keyword
+            'keyword': self.keyword,
+            'count': 10,
+            'page': pages
         }
 
         headers = {
@@ -29,23 +31,25 @@ class SuperJobAPI(AbstractClassAPI):
         response = requests.get(self.url, params=query_parameters, headers=headers)
         return response.json()
 
-    def save_data(self):
-        data = self.get_vacancies()
-        with open(self.path_to_json, 'w', encoding='utf-8') as file:
+    def save_data(self, pages=0):
+        with open(self.path_to_json, 'a', encoding='utf-8') as file:
             lst = []
-            for vacancy in data['objects']:
+            for page in range(pages):
+                data = self.get_vacancies(pages)
+                for vacancy in data['objects']:
 
-                requirements = vacancy['candidat'][:100] + '...'
+                    requirements = vacancy['candidat'][:100] + '...'
 
-                lst.append({
-                    'name': vacancy['profession'],
-                    'url': vacancy['link'],
-                    'salary': vacancy['payment_from'],
-                    'experience': vacancy['experience']['title'],
-                    'requirements': requirements,
-                    'city': vacancy['town']['title']
-                })
+                    lst.append({
+                        'name': vacancy['profession'],
+                        'url': vacancy['link'],
+                        'salary': vacancy['payment_from'],
+                        'experience': vacancy['experience']['title'],
+                        'requirements': requirements,
+                        'city': vacancy['town']['title']
+                    })
 
+            self.clear_data()
             json.dump(lst, file, indent=4, ensure_ascii=False)
 
 
